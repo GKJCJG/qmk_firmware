599a845f334e89a904b2a680bb72bcba6a8bf323
Colin Grant <cojgrant@gmail.com>
2020-06-07 00:12:50 -0500

    greek compiling, russian isn't

---
+++ keyboards/ergodox_ez/keymaps/GKJCJG/custom_keycodes.c
@@ -0,0 +1,174 @@
+enum custom_keycodes {
+  PLACEHOLDER = SAFE_RANGE,
+  GKJ_ACUTE, // /
+  GKJ_GRAVE, // g
+  GKJ_CFLX, // c
+  GKJ_MCRN, // RShft
+  GKJ_TREMA, // -
+  GKJ_BAR, // '
+  GKJ_BRV, // x
+  GKJ_HACEK, // v
+  GKJ_TILDE, // n
+  GKJ_DOT, // .
+  GKJ_RING, // r
+  GKJ_RBLW, // ->
+  GKJ_SMVL, // z
+  GKJ_UBLW, // y
+  GKJ_CDLA, // <-
+  GKJ_OGNK, // ,
+  GKJ_SMTH, // LTHUMB
+  GKJ_ROUGH, // LTHUMB
+  GKJ_IOTA, // LTHUMB
+
+  GKJ_ASH, // a
+  GKJ_U_ASH,
+  GKJ_OE, // q
+  GKJ_U_OE,
+  GKJ_OBAR, // o
+  GKJ_U_OBAR,
+  GKJ_ETH, // d
+  GKJ_U_ETH,
+  GKJ_THRN, // t
+  GKJ_U_THRN,
+  GKJ_SZ, // s
+  GKJ_U_SZ,
+  GKJ_LBAR, // l
+  GKJ_U_LBAR,
+  GKJ_HBAR, // h
+  GKJ_U_HBAR,
+  GKJ_BBAR, // b
+  GKJ_U_BBAR,
+  GKJ_IDOT, // i
+  GKJ_U_IDOT,
+
+
+  GK_A,
+  GK_U_A,
+  GK_B,
+  GK_U_B,
+  GK_G,
+  GK_U_G,
+  GK_D,
+  GK_U_D,
+  GK_E,
+  GK_U_E,
+  GK_Z,
+  GK_U_Z,
+  GK_H,
+  GK_U_H,
+  GK_TH,
+  GK_U_TH,
+  GK_I,
+  GK_U_I,
+  GK_K,
+  GK_U_K,
+  GK_L,
+  GK_U_L,
+  GK_M,
+  GK_U_M,
+  GK_N,
+  GK_U_N,
+  GK_KS,
+  GK_U_KS,
+  GK_O,
+  GK_U_O,
+  GK_P,
+  GK_U_P,
+  GK_R,
+  GK_U_R,
+  GK_S,
+  GK_U_S,
+  GK_SF,
+  GK_U_SF,
+  GK_T,
+  GK_U_T,
+  GK_Y,
+  GK_U_Y,
+  GK_PH,
+  GK_U_PH,
+  GK_KH,
+  GK_U_KH,
+  GK_PS,
+  GK_U_PS,
+  GK_OO,
+  GK_U_OO,
+  GK_W,
+  GK_U_W,
+  GK_HH,
+  GK_U_HH,
+  
+  R_A, //
+  R_U_A,
+  R_B, //
+  R_U_B,
+  R_V, //
+  R_U_V,
+  R_G, //
+  R_U_G,
+  R_D, //
+  R_U_D,
+  R_JE, //
+  R_U_JE,
+  R_E, //
+  R_U_E,
+  R_ZH, //
+  R_U_ZH,
+  R_Z, //
+  R_U_Z,
+  R_I, //
+  R_U_I,
+  R_K, //
+  R_U_K,
+  R_L, //
+  R_U_L,
+  R_M, //
+  R_U_M,
+  R_N, //
+  R_U_N,
+  R_O, //
+  R_U_O,
+  R_P, //
+  R_U_P,
+  R_R, //
+  R_U_R,
+  R_S, //
+  R_U_S,
+  R_T, //
+  R_U_T,
+  R_U, //
+  R_U_U,
+  R_F, //
+  R_U_F,
+  R_H, //
+  R_U_H,
+  R_C, //
+  R_U_C,
+  R_CH, //
+  R_U_CH,
+  R_SH, //
+  R_U_SH,
+  R_SHCH, //
+  R_U_SHCH,
+  R_HA, // HART SIGN
+  R_U_HA,
+  R_Y, //
+  R_U_Y,
+  R_SO, // SOFT SIGN
+  R_U_SO,
+  R_YU, //
+  R_U_YU,
+  R_YA, //
+  R_U_YA,
+  R_IA, //
+  R_U_IA,
+  R_IE, //
+  R_U_IE,
+  R_EN, //
+  R_U_EN,
+  R_ON, //
+  R_U_ON,
+  R_IEN, //
+  R_U_IEN,
+  R_ION, //
+  R_U_ION
+};
